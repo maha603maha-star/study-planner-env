c10032cbd992a90b2d0f5c907f7345ac1159fd50
Mahalakshmi M <maha603maha@gmail.com>
2026-04-07 00:22:11 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,1 +1,18 @@
-print("Submission working")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def run():
+    try:
+        data = request.get_json(force=True)
+        return jsonify({
+            "output": "Success"
+        })
+    except:
+        return jsonify({
+            "output": "Error"
+        })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
